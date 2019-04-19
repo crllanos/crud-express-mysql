871a6581aad89b354f6f3f138a0e9dbc1b06d4c4
cristian llanos <cristian.llanos@gmail.com>
2019-04-19 04:53:58 -0400

    ADD: Render page with user data from db.

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -2,11 +2,11 @@ const controller = {}
 
 controller.list = (req, res) => {
     req.getConnection((err, conn) =>{
-        if(err) throw new Error(err);
+        if(err) res.json(db_err);
         conn.query("SELECT * FROM customers", (db_err, customers)=>{
             if(db_err) res.json(db_err);
-            console.log(customers)
-            res.send(customers)
+            res.render('customers', {customers});
+            //res.send(customers)
         });
     })
 }
